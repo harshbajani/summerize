fb5fe0419ebc03f597576043fbb2b9cd5669325f
unknown <harshbajani7@gmail.com>
2023-06-16 10:59:14 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,10 @@ const Hero = () => {
         <button
           type="button"
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open(
+              "https://github.com/harshbajani/summerize.git",
+              "_blank"
+            )
           }
           className="black_btn"
         >
